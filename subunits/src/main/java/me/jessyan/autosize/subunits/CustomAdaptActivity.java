cc3bfafb8120d6a8a2f492fd16fb2e57539cf150
Farhan Rahman Arnob <this.farhanarnob@gmail.com>
2023-02-16 16:39:09 -0600

    package update

--- subunits/src/main/java/me/jessyan/autosize/subunits/CustomAdaptActivity.java
+++ subunits/src/main/java/me/jessyan/autosize/subunits/CustomAdaptActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.jessyan.autosize.demo.subunits;
+package me.jessyan.autosize.subunits;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
--- subunits/src/main/java/me/jessyan/autosize/subunits/MainActivity.java
+++ subunits/src/main/java/me/jessyan/autosize/subunits/MainActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.jessyan.autosize.demo.subunits;
+package me.jessyan.autosize.subunits;
 
 import android.app.Activity;
 import android.content.Intent;
